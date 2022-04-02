2ae924f5eb01a7857795eea2e41f83ef3ca93849
Hong Anh <honganh@example.com>
2022-04-02 22:55:42 +0700

    combine makeRows and makeCols next

--- grid.js
+++ grid.js
@@ -3,26 +3,26 @@
 const container = document.getElementById("container");
 let rows = document.getElementsByClassName("gridRow");
 let cells = document.getElementsByClassName("gridCell");
-
+let slider = document.querySelector("#sizeRange")
 // create buttons
 const btnReset = document.getElementById("reset")
-const btnBlack = document.getElementById("black")
-const btnRainbow = document.getElementById("rainbow")
-
+const btnSColor = document.querySelectorAll(".color-choice")
+const btnColorPicker = document.querySelector("#input-color");
 // change the trail color
 let currentColor = 'black'
-const colors = { black: 'black' }
-e => e.target.classList.add(colors[currentColor])
 
+// FUNCTIONS
 // Creates a default grid sized 16x16
-function createGrid() {
-  makeRows(16);
-  makeColumns(16);
+function createGrid(num) {
+  makeRows(num);
+  makeColumns(num);
+  // Hover mouse over to draw
+  let gridPixels = container.querySelectorAll("div");
+  gridPixels.forEach(gridPixel => gridPixel.addEventListener('mouseover', colorGrid));
 }
 
 // Takes (rows, columns) input and makes a grid
 function makeRows(rowNum) {
-
   // Creates rows
   for (r = 0; r < rowNum; r++) {
     let row = document.createElement("div");
@@ -35,19 +35,102 @@ function makeColumns(cellNum) {
   for (i = 0; i < rows.length; i++) {
     for (j = 0; j < cellNum; j++) {
       let newCell = document.createElement("div");
-      newCell.addEventListener('mouseover',
-        e => e.target.classList.add('my-color-class')
-      )
       rows[j].appendChild(newCell).className = "gridCell";
     };
   };
 };
 
-// change grid square color to red
-function changeColor() {
-  this.style.backgroundColor = '#ff9999';
+// color options + eraser
+function colorGrid() {
+  switch (color) {
+    case "rainbow":
+      this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+      this.classList.remove("gray");
+      break;
+    case "gray":
+      if (this.style.backgroundColor.match(/rgba/)) {
+        let currentOpacity = Number(this.style.backgroundColor.slice(-4, -1));
+        if (currentOpacity <= 0.9) {
+          this.style.backgroundColor = `rgba(0, 0, 0, ${currentOpacity + 0.1})`;
+          this.classList.add('gray');
+        }
+      }
+      else if (this.classList == 'gray' && this.style.backgroundColor == 'rgb(0, 0, 0') {
+        return;
+      }
+      else {
+        this.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
+      }
+      break;
+    case "eraser":
+      this.style.backgroundColor = "#ffffff";
+      this.classList.remove('gray');
+      break;
+    case "black":
+      this.style.backgroundColor = "#000000";
+      this.classList.remove('gray');
+      break;
+    default:
+      this.style.backgroundColor = currentColor;
+      this.classList.remove('gray');
+      break;
+  }
 }
 
+// clear grid
+function eraseAllColor() {
+  let gridPixels = container.querySelectorAll("div");
+  gridPixels.forEach(gridPixel => gridPixel.style.backgroundColor = "#ffffff");
+}
 
-createGrid();
+// update color button when a color is picked
+function changeColor(event) {
+  switch (event.target.dataset.color) {
+    case "rainbow":
+      color = "rainbow";
+      break;
+    case "rainbow":
+      color = "rainbow";
+      break;
+    case "gray":
+      color = "gray";
+      break;
+    case "eraser":
+      color = "eraser";
+      break;
+    default:
+      color = "black";
+      break;
+  }
+}
+function pixelSize() {
+  let gridPixels = container.querySelectorAll('div');
+  gridPixels.forEach(gridPixel => gridPixel.remove());
+  createGrid(slider.value);
+}
+function userColorSelection(event) {
+  color = event.target.value;
+}
+
+function buttonHover() {
+  this.style.border = '1px solid #ffffff';
+}
+
+function buttonStandard() {
+  this.style.border = '1px solid #FF0000';
+}
+// EXECUTIONS
+// On Page Load
+createGrid(16);
+// Event listeners
+// 3 modes each btn: click/over/out
+btnReset.addEventListener('click', eraseAllColor);
+btnReset.addEventListener('mouseover', buttonHover);
+btnReset.addEventListener('mouseout', buttonStandard);
+btnSColor.forEach(btnColor => btnColor.addEventListener('click', changeColor));
+btnSColor.forEach(btnColor => btnColor.addEventListener('mouseover', buttonHover));
+btnSColor.forEach(btnColor => btnColor.addEventListener('mouseout', buttonStandard));
+slider.addEventListener('mouseup', pixelSize);
+btnColorPicker.addEventListener('change', userColorSelection, false);
+btnColorPicker.addEventListener('input', userColorSelection, false);
 
