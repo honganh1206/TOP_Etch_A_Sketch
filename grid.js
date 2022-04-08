5e67645a55f8a2319cb880d9eff13b765bb3fc84
Hong Anh <honganh@example.com>
2022-04-08 11:15:57 +0700

    delete unnecessary functions, btnReset.addEvent is not a function?

--- grid.js
+++ grid.js
@@ -5,7 +5,7 @@ let rows = document.getElementsByClassName("gridRow");
 let cells = document.getElementsByClassName("gridCell");
 let slider = document.querySelector("#sizeRange")
 // create buttons
-const btnReset = document.getElementById("reset")
+const btnReset = document.getElementsByClassName("reset")
 const btnSColor = document.querySelectorAll(".color-choice")
 const btnColorPicker = document.querySelector("#input-color");
 // change the trail color
@@ -23,90 +23,28 @@ function createGrid(rowNum, colNum) {
     for (j = 0; j < colNum; j++) {
       let newCells = document.createElement("div");
       rows[j].appendChild(newCells).className = "gridCell";
-      //   newCells.addEventListener('mouseover',
-      //     e => e.target.classList.add('my-color'))
+      newCells.addEventListener('mouseover',
+        e => e.target.classList.add('my-color'))
     };
-  let gridPixels = document.getElementsByClassName("gridCell");
-  gridPixels.forEach(gridPixel => gridPixel.addEventListener('mouseover', colorGrid))
+  // this does not work somehowv
+  // let gridPixels = container.querySelectorAll("div");
+  // Array.from(gridPixels).forEach(gridPixel => {
+  //   addEventListener('mouseover', colorGrid)
+  // })
+  // Testing => empty string
+  // Test with typeof => still working
+  // let gridPixels = document.getElementsByClassName("gridCell")
+  // for (i = 0; i < gridPixels.length; i++) {
+  //   console.log(typeof gridPixels[i])
+  //  }
 };
-// color options + eraser
-function colorGrid() {
-  switch (currentColor) {
-    case "rainbow":
-      this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-      this.classList.remove("gray");
-      break;
-    case "gray":
-      if (this.style.backgroundColor.match(/rgba/)) {
-        let currentOpacity = Number(this.style.backgroundColor.slice(-4, -1));
-        if (currentOpacity <= 0.9) {
-          this.style.backgroundColor = `rgba(0, 0, 0, ${currentOpacity + 0.1})`;
-          this.classList.add('gray');
-        }
-      }
-      else if (this.classList == 'gray' && this.style.backgroundColor == 'rgb(0, 0, 0') {
-        return;
-      }
-      else {
-        this.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
-      }
-      break;
-    case "eraser":
-      this.style.backgroundColor = "#ffffff";
-      this.classList.remove('gray');
-      break;
-    case "black":
-      this.style.backgroundColor = "#000000";
-      this.classList.remove('gray');
-      break;
-    default:
-      this.style.backgroundColor = currentColor;
-      this.classList.remove('gray');
-      break;
-  }
-}
-
 // clear grid
 function eraseAllColor() {
-  let gridPixels = container.querySelectorAll("div");
-  gridPixels.forEach(gridPixel => gridPixel.style.backgroundColor = "#ffffff");
-}
-
-// update color button when a color is picked
-function changeColor(event) {
-  switch (event.target.dataset.color) {
-    case "rainbow":
-      color = "rainbow";
-      break;
-    case "rainbow":
-      color = "rainbow";
-      break;
-    case "gray":
-      color = "gray";
-      break;
-    case "eraser":
-      color = "eraser";
-      break;
-    default:
-      color = "black";
-      break;
+  let gridPixels = document.getElementsByClassName("gridCell");
+  for (let i = 0; i < gridPixels.length; i++) {
+    // gridPixels[i].style.backgroundColor = "#ffffff"
+    console.log(typeof gridPixels[i])
   }
-}
-function pixelSize() {
-  let gridPixels = container.querySelectorAll('div');
-  gridPixels.forEach(gridPixel => gridPixel.remove());
-  createGrid(slider.value);
-}
-function userColorSelection(event) {
-  color = event.target.value;
-}
-
-function buttonHover() {
-  this.style.border = '1px solid #ffffff';
-}
-
-function buttonStandard() {
-  this.style.border = '1px solid #FF0000';
 }
 // EXECUTIONS
 // On Page Load
@@ -114,12 +52,12 @@ createGrid(16, 16);
 // Event listeners
 // 3 modes each btn: click/over/out
 btnReset.addEventListener('click', eraseAllColor);
-btnReset.addEventListener('mouseover', buttonHover);
-btnReset.addEventListener('mouseout', buttonStandard);
-btnSColor.forEach(btnColor => btnColor.addEventListener('click', changeColor));
-btnSColor.forEach(btnColor => btnColor.addEventListener('mouseover', buttonHover));
-btnSColor.forEach(btnColor => btnColor.addEventListener('mouseout', buttonStandard));
-slider.addEventListener('mouseup', pixelSize);
-btnColorPicker.addEventListener('change', userColorSelection, false);
-btnColorPicker.addEventListener('input', userColorSelection, false);
+// btnReset.addEventListener('mouseover', buttonHover);
+// btnReset.addEventListener('mouseout', buttonStandard);
+// btnSColor.forEach(btnColor => btnColor.addEventListener('click', changeColor));
+// btnSColor.forEach(btnColor => btnColor.addEventListener('mouseover', buttonHover));
+// btnSColor.forEach(btnColor => btnColor.addEventListener('mouseout', buttonStandard));
+// slider.addEventListener('mouseup', pixelSize);
+// btnColorPicker.addEventListener('change', userColorSelection, false);
+// btnColorPicker.addEventListener('input', userColorSelection, false);
 
